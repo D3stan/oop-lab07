0e97a9e9008600cd5ab244bea3ef857eedd7c6ba
Alessandro <alessandro.porcheddu@studio.unibo.it>
2024-11-05 17:07:11 +0100

    end lab time

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -4,6 +4,7 @@ import it.unibo.functional.api.Function;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
@@ -54,7 +55,13 @@ public final class Transformers {
      * @param <O> output elements type
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        Function<I, ? extends Collection<? extends O>> singleTrasformer = new Function<I,Collection<? extends O>>() {
+            @Override
+            public Collection<? extends O> call(final I input) {
+                return List.of(transformer.call(input));
+            }
+        };
+        return flattenTransform(base, singleTrasformer);
     }
 
     /**
@@ -70,7 +77,7 @@ public final class Transformers {
      * @param <I> type of the collection elements
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
